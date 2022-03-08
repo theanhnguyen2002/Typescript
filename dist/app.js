bdbdb9f609abcc9a2cd98f71582f90da30bf3a52
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-03-08 13:47:29 +0700

    Buổi 1: TypeScript

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const a = 20;
+const b = 30;
+const name = "Nguyen The Anh";
+const age = 20;
+const status = true;
+const info = { id: 1, name: "Nguyen The Anh" };
+const stringArr = ["a", "b", "c"];
+const numberArr = [1, 2, 3, 4, 5];
+const objectArr = [{ id: 1 }, { id: 2 }];
+function sum(numA, numB) {
+    return numA + numB;
+}
+sum(a, b);
