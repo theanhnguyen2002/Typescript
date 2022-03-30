1a01d787305d683e4201b79f6a2e0d47fb252d88
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-03-31 01:40:09 +0700

    Update

--- src/pages/layouts/AdminLayout.tsx
+++ src/pages/layouts/AdminLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../../components/Sidebar';
-import '.AdminLayout.css';
+import './AdminLayout.css';
 
 type Props = {}
 
