a9b9e50ac9bc48ad0e25e39f39e77267a24a08a8
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-04-03 02:27:26 +0700

    Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -124,55 +124,47 @@ const Home = (props: Props) => {
                     </div>
                     <ul className="timeline">
                         <li>
-                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="..." /></div>
+                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1648926831/2971606c7a4764be6b94aa71f63b5f3c_umzhxh.jpg"/></div>
                         <div className="timeline-panel">
                             <div className="timeline-heading">
-                            <h4>2009-2011</h4>
-                            <h4 className="subheading">Our Humble Beginnings</h4>
+                            <h4>March 30, 1971, </h4>
+                            <h4 className="subheading">the first Starbucks coffee shop was founded by 3 people</h4>
                             </div>
-                            <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            <div className="timeline-body"><p className="text-muted">English teacher Jerry Baldwin, history teacher Zev Siegl, and writer Gordon Bowker.</p></div>
                         </div>
                         </li>
                         <li className="timeline-inverted">
-                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/2.jpg" alt="..." /></div>
+                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1648927261/bai-hoc-kinh-doanh-ca-phe-tu-ceo-starbucks-howard-schultz_cwvc1h.jpg" alt="..." /></div>
                         <div className="timeline-panel">
                             <div className="timeline-heading">
-                            <h4>March 2011</h4>
-                            <h4 className="subheading">An Agency is Born</h4>
+                            {/* <h4>March 2011</h4> */}
+                            <h4 className="subheading">Inspirationn</h4>
                             </div>
-                            <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            <div className="timeline-body"><p className="text-muted">Inspired by Alfred Peet, founder of Peet's Coffee and Tea brand. Some time later, the shop moved to 1912 Pike Place, where it still exists today, and we also started buying beans directly from the farms.</p></div>
                         </div>
                         </li>
                         <li>
-                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="..." /></div>
+                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1648927264/4_KCSE_yg7gve.jpg" alt="..." /></div>
                         <div className="timeline-panel">
                             <div className="timeline-heading">
-                            <h4>December 2015</h4>
-                            <h4 className="subheading">Transition to Full Service</h4>
+                            {/* <h4>December 2015</h4> */}
+                            <h4 className="subheading">Starbucks brand name origin</h4>
                             </div>
-                            <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            <div className="timeline-body"><p className="text-muted">Originally, the studio intended to name it Pequod, inspired by the novel Moby-Dick. However, after the name was rejected by one of the co-founders, the company was named Starbucks, a character from the novel above.</p></div>
                         </div>
                         </li>
                         <li className="timeline-inverted">
-                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/4.jpg" alt="..." /></div>
+                        <div className="timeline-image"><img className="rounded-circle img-fluid" src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1648927363/starbucks-coffee-24-giaoduc.vn.net_copy_r42dwc.jpg" alt="..." /></div>
                         <div className="timeline-panel">
                             <div className="timeline-heading">
-                            <h4>July 2020</h4>
-                            <h4 className="subheading">Phase Two Expansion</h4>
+                            {/* <h4>July 2020</h4> */}
+                            <h4 className="subheading">Development process</h4>
                             </div>
-                            <div className="timeline-body"><p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p></div>
+                            <div className="timeline-body"><p className="text-muted">Established on March 30, 1971 in Seattle. Howard Schultz joined the company in 1982 as Director of Retail Operations and Marketing. Schultz started the Il Giornale coffee bar chain in April 1986. In 1984, the original owners of Starbucks, led by Baldwin, took the opportunity to buy Peet (Baldwin still operates there). During the 1980s, Starbucks provided the best benefits to our employees.</p></div>
                         </div>
                         </li>
                         <li className="timeline-inverted">
-                        <div className="timeline-image">
-                            <h4>
-                            Be Part
-                            <br />
-                            Of Our
-                            <br />
-                            Story!
-                            </h4>
-                        </div>
+                            <div className="timeline-image"><img className="rounded-circle img-fluid" src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1648927527/starbucks-coffee-logo_phzvh9.gif" alt="..." /></div>
                         </li>
                     </ul>
                     </div>
