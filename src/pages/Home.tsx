3e5d910080e5817cf80efb6b9014a924f0c8b867
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-04-03 02:54:17 +0700

    Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,7 +26,7 @@ const Home = (props: Props) => {
                             <a className="btn-xl text-uppercase" id="btn-banner" href="#services">See more</a>
                     </div>
                 </header>
-                {/* Services*/}
+                {/* 123*/}
                 <section className="page-section" id="services">
                     <div className="container">
                     <div className="text-center">
@@ -58,7 +58,7 @@ const Home = (props: Props) => {
                     </div>
                     </div>
                 </section>
-                {/* Portfolio Grid*/}
+                {/* Category*/}
                 <section className="page-section bg-light" id="portfolio">
                     <div className="container">
                     <div className="text-center">
@@ -237,21 +237,26 @@ const Home = (props: Props) => {
                     </div>
                 </section>
                 
-                {/* Portfolio item 1 modal popup*/}
-                <div className="portfolio-modal modal fade" id="portfolioModal1" tabIndex={-1} role="dialog" aria-hidden="true">
+                {/* Category item 1 modal popup*/}
+                    <div className="portfolio-modal modal fade" id="portfolioModal1" tabIndex={-1} role="dialog" aria-hidden="true">
                     <div className="modal-dialog">
                     <div className="modal-content">
-                        <div className="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                                <div className="close-modal" data-bs-dismiss="modal">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" />
+                                    <path fillRule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z" />
+                                    </svg>
+                                </div>
                         <div className="container">
                         <div className="row justify-content-center">
                             <div className="col-lg-8">
                             <div className="modal-body">
                                 {/* Project details*/}
-                                <h2 className="text-uppercase">Project Name</h2>
-                                <p className="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img className="img-fluid d-block mx-auto" src="assets/img/portfolio/1.jpg" alt="..." />
-                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                <ul className="list-inline">
+                                <h2 className="text-uppercase">Coffees</h2>
+                                <p className="item-intro text-muted">Drinks</p>
+                                <img className="img-fluid d-block mx-auto" src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1648922965/sbx20190617-33269-caffeamericano-ongreen-corelib-srgb_sfkl5o.jpg" alt="..." />
+                                <p>Our Coffee masters have combined their years of Coffee tasting experience in three simple questions to help you find the coffee you're sure to love.</p>
+                                {/* <ul className="list-inline">
                                 <li>
                                     <strong>Client:</strong>
                                     Threads
@@ -260,10 +265,10 @@ const Home = (props: Props) => {
                                     <strong>Category:</strong>
                                     Illustration
                                 </li>
-                                </ul>
+                                </ul> */}
                                 <button className="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
                                 <i className="fas fa-xmark me-1" />
-                                Close Project
+                                See details
                                 </button>
                             </div>
                             </div>
@@ -272,33 +277,29 @@ const Home = (props: Props) => {
                     </div>
                     </div>
                 </div>
-                {/* Portfolio item 2 modal popup*/}
+                {/* Category item 2 modal popup*/}
                 <div className="portfolio-modal modal fade" id="portfolioModal2" tabIndex={-1} role="dialog" aria-hidden="true">
                     <div className="modal-dialog">
                     <div className="modal-content">
-                        <div className="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                                <div className="close-modal" data-bs-dismiss="modal">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" />
+                                        <path fillRule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z" />
+                                    </svg>
+                                </div>
                         <div className="container">
                         <div className="row justify-content-center">
                             <div className="col-lg-8">
                             <div className="modal-body">
                                 {/* Project details*/}
-                                <h2 className="text-uppercase">Project Name</h2>
-                                <p className="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img className="img-fluid d-block mx-auto" src="assets/img/portfolio/2.jpg" alt="..." />
-                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                <ul className="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Explore
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Graphic Design
-                                </li>
-                                </ul>
+                                <h2 className="text-uppercase">Teas</h2>
+                                <p className="item-intro text-muted">Drinks</p>
+                                <img className="img-fluid d-block mx-auto" src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1648922965/sbx20190624-38221-royalenglishbreakfastblacktea-ongreen-corelib-g7op_ds3trp.jpg" alt="..." />
+                                <p>Our Teas masters have combined their years of Teas tasting experience into three simple questions to help you find the Teas you're sure to love.</p>
+                                
                                 <button className="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
                                 <i className="fas fa-xmark me-1" />
-                                Close Project
+                                See details
                                 </button>
                             </div>
                             </div>
@@ -307,33 +308,29 @@ const Home = (props: Props) => {
                     </div>
                     </div>
                 </div>
-                {/* Portfolio item 3 modal popup*/}
+                {/* Category item 3 modal popup*/}
                 <div className="portfolio-modal modal fade" id="portfolioModal3" tabIndex={-1} role="dialog" aria-hidden="true">
                     <div className="modal-dialog">
                     <div className="modal-content">
-                        <div className="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
+                                <div className="close-modal" data-bs-dismiss="modal">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" />
+                                        <path fillRule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z" />
+                                    </svg>
+                                </div>
                         <div className="container">
                         <div className="row justify-content-center">
                             <div className="col-lg-8">
                             <div className="modal-body">
                                 {/* Project details*/}
-                                <h2 className="text-uppercase">Project Name</h2>
-                                <p className="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img className="img-fluid d-block mx-auto" src="assets/img/portfolio/3.jpg" alt="..." />
-                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                <ul className="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Finish
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Identity
-                                </li>
-                                </ul>
+                                <h2 className="text-uppercase">Sandwiches</h2>
+                                <p className="item-intro text-muted">Food</p>
+                                <img className="img-fluid d-block mx-auto" src="https://res.cloudinary.com/dsirnbuyv/image/upload/v1648923331/starbucks-breakfast-sandwich_ctzp1o.jpg" alt="..." />
+                                <p>Our Sandwiches masters have combined their years of Sandwiches tasting experience into three simple questions to help you find the Sandwiches you're sure to love.</p>
+                                
                                 <button className="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
                                 <i className="fas fa-xmark me-1" />
-                                Close Project
+                                See details
                                 </button>
                             </div>
                             </div>
@@ -342,111 +339,7 @@ const Home = (props: Props) => {
                     </div>
                     </div>
                 </div>
-                {/* Portfolio item 4 modal popup*/}
-                <div className="portfolio-modal modal fade" id="portfolioModal4" tabIndex={-1} role="dialog" aria-hidden="true">
-                    <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
-                        <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-lg-8">
-                            <div className="modal-body">
-                                {/* Project details*/}
-                                <h2 className="text-uppercase">Project Name</h2>
-                                <p className="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img className="img-fluid d-block mx-auto" src="assets/img/portfolio/4.jpg" alt="..." />
-                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                <ul className="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Lines
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Branding
-                                </li>
-                                </ul>
-                                <button className="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i className="fas fa-xmark me-1" />
-                                Close Project
-                                </button>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                {/* Portfolio item 5 modal popup*/}
-                <div className="portfolio-modal modal fade" id="portfolioModal5" tabIndex={-1} role="dialog" aria-hidden="true">
-                    <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
-                        <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-lg-8">
-                            <div className="modal-body">
-                                {/* Project details*/}
-                                <h2 className="text-uppercase">Project Name</h2>
-                                <p className="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img className="img-fluid d-block mx-auto" src="assets/img/portfolio/5.jpg" alt="..." />
-                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                <ul className="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Southwest
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Website Design
-                                </li>
-                                </ul>
-                                <button className="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i className="fas fa-xmark me-1" />
-                                Close Project
-                                </button>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                {/* Portfolio item 6 modal popup*/}
-                <div className="portfolio-modal modal fade" id="portfolioModal6" tabIndex={-1} role="dialog" aria-hidden="true">
-                    <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="close-modal" data-bs-dismiss="modal"><img src="assets/img/close-icon.svg" alt="Close modal" /></div>
-                        <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-lg-8">
-                            <div className="modal-body">
-                                {/* Project details*/}
-                                <h2 className="text-uppercase">Project Name</h2>
-                                <p className="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
-                                <img className="img-fluid d-block mx-auto" src="assets/img/portfolio/6.jpg" alt="..." />
-                                <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!</p>
-                                <ul className="list-inline">
-                                <li>
-                                    <strong>Client:</strong>
-                                    Window
-                                </li>
-                                <li>
-                                    <strong>Category:</strong>
-                                    Photography
-                                </li>
-                                </ul>
-                                <button className="btn btn-primary btn-xl text-uppercase" data-bs-dismiss="modal" type="button">
-                                <i className="fas fa-xmark me-1" />
-                                Close Project
-                                </button>
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
+                
                 </div>
 
 
