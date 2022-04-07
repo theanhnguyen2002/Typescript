39af4dbd40edbc84fc508f6ac4cd6e3f32c35109
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-04-08 00:27:31 +0700

    Update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 import './vendor/jquery/jquery.min.js';
-import './vendor/bootstrap/js/bootstrap.bundle.min.js';
-import './vendor/jquery-easing/jquery.easing.min.js';
+// import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+// import './vendor/jquery-easing/jquery.easing.min.js';
 import './js/sb-admin-2.min.js';
 import './vendor/chart.js/Chart.min.js';
 import './css/sb-admin-2.min.css';
