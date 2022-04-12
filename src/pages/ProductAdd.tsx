b7718079ebb21605d496556a18f0bca5f292f102
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-04-12 15:47:27 +0700

    Uodate: Layout add, edit

--- src/pages/ProductAdd.tsx
+++ src/pages/ProductAdd.tsx
@@ -20,8 +20,8 @@ const ProductAdd = (props: ProductAddProps) => {
     const navigate = useNavigate();
     const [url, setUrl] = useState<string>('');
     const onSubmit: SubmitHandler<TypeInputs> = data => {
-        
-        data.img=url;
+
+        data.img = url;
         // console.log(data)
         props.onAdd(data);
         navigate("/admin/products")
@@ -47,14 +47,49 @@ const ProductAdd = (props: ProductAddProps) => {
     }
     const imgField = register('img', { required: true });
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            {props.name}
-            <input type="text" placeholder='Ten san pham' {...register('name')} />
-            <input type="file" placeholder='Anh san pham' {...imgField} onChange={(e) => { imgField.onChange(e); imgProduct(e) }} />
-            <input type="number" placeholder='Gia san pham' {...register('price')} />
-            
-            <button>Add</button>
-        </form>
+
+        <div className="container-xxl flex-grow-1 container-p-y">
+            <h4 className="fw-bold py-3 mb-4"><span className="text-muted fw-light">Product /</span> Add</h4>
+            <div className="row">
+                <div className="col-xl">
+                    <div className="card mb-4">
+                        <div className="card-header d-flex justify-content-between align-items-center">
+                            <h5 className="mb-0">Add</h5>
+                        </div>
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                {props.name}
+                                <div className="mb-3">
+                                    <label className="form-label" htmlFor="basic-default-fullname">Name Product</label>
+                                    <input type="text" className="form-control" id="basic-default-fullname" placeholder="name" {...register('name')}/>
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label" htmlFor="basic-default-fullname">Photo Product</label>
+                                    <input type="file" className="form-control" id="basic-default-fullname" {...imgField} onChange={(e) => { imgField.onChange(e); imgProduct(e) }}/>
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label" htmlFor="basic-default-email">Price Product</label>
+                                    <div className="input-group input-group-merge">
+                                        <input type="text" id="basic-default-email" className="form-control" placeholder="price" {...register('price')}/>
+                                        <span className="input-group-text" id="basic-default-email2">$</span>
+                                    </div>
+                                </div>
+
+                                {/* <div className="mb-3">
+                                    <label className="form-label" htmlFor="basic-default-message">desc</label>
+                                    <textarea id="basic-default-message" className="form-control" placeholder="Hi, Do you have a moment to talk Joe?" defaultValue={""} />
+                                </div> */}
+                                <button className="btn btn-primary">Add</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
     )
 }
 
--- src/pages/ProductEdit.tsx
+++ src/pages/ProductEdit.tsx
@@ -85,12 +85,45 @@ const ProductEdit = (props: ProductEditProps) => {
     // const imgField = register('img', { required: true });
 
     return (
-        <form action="" onSubmit={handleSubmit(onSubmit)}>
-            <input type="text"  {...register('name', { required: true })} />
-            <input type="file"  {...register('img')} />
-            <input type="number"  {...register("price")} />
-            <button>Update</button>
-        </form>
+
+
+        <div className="container-xxl flex-grow-1 container-p-y">
+            <h4 className="fw-bold py-3 mb-4"><span className="text-muted fw-light">Product /</span> Add</h4>
+            <div className="row">
+                <div className="col-xl">
+                    <div className="card mb-4">
+                        <div className="card-header d-flex justify-content-between align-items-center">
+                            <h5 className="mb-0">Add</h5>
+                        </div>
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className="mb-3">
+                                    <label className="form-label" htmlFor="basic-default-fullname">Name Product</label>
+                                    <input type="text" className="form-control" id="basic-default-fullname" placeholder="name" {...register('name', { required: true })} />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label" htmlFor="basic-default-fullname">Photo Product</label>
+                                    <input type="file" className="form-control" id="basic-default-fullname" {...register('img')} />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label" htmlFor="basic-default-email">Price Product</label>
+                                    <div className="input-group input-group-merge">
+                                        <input type="text" id="basic-default-email" className="form-control" placeholder="price" {...register("price")} />
+                                        <span className="input-group-text" id="basic-default-email2">$</span>
+                                    </div>
+                                </div>
+
+                                {/* <div className="mb-3">
+                                    <label className="form-label" htmlFor="basic-default-message">desc</label>
+                                    <textarea id="basic-default-message" className="form-control" placeholder="Hi, Do you have a moment to talk Joe?" defaultValue={""} />
+                                </div> */}
+                                <button className="btn btn-primary">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
